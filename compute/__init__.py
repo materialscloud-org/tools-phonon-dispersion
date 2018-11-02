bcf7a6619b297af786fda178bbead1ab383ce156
Snehal Kumbhar <snehal.kumbhar@epfl.ch>
2018-11-02 19:15:34 +0100

    added redirection to selection page

--- compute/__init__.py
+++ compute/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import flask
 from flask import Blueprint
-import io
+
 blueprint = Blueprint('compute', __name__, url_prefix='/compute')
 
 logger = logging.getLogger('tools-app')
@@ -23,9 +23,9 @@ def process_structure():
             return flask.redirect(flask.url_for('input_data'))
 
     else:
-        return flask.redirect(flask.url_for('compute.process_structure_example'))
-        #flask.flash("Redirecting...")
-        #return flask.redirect(flask.url_for('input_data'))
+        #return flask.redirect(flask.url_for('compute.process_structure_example'))
+        return flask.redirect("/")
+        return flask.redirect(flask.url_for('input_data'))
 
 @blueprint.route('/process_example_structure/', methods=['GET', 'POST'])
 def process_structure_example():
@@ -34,6 +34,6 @@ def process_structure_example():
         return flask.render_template("user_templates/visualizer.html", structure=examplestructure)
         #return "This was a POST " + examplestructure
     else:
-        return flask.redirect(flask.url_for('compute.process_structure_example'))
+        return flask.redirect("/")
         #return flask.render_template("user_templates/visualizer.html", structure="C2")
         #return "This was a GET"
