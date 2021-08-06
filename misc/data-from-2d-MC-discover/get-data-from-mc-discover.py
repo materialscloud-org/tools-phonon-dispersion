a5bb5396fc2639b245774d671946c9dd3b11a98b
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2021-08-06 18:08:58 +0200

    Extracting the SCF via the phonons from MC
    I update the script so it gets the SCF input via the parents of the phonons,
    rather than from the parents of the bands, that sometimes are just vc-relax.
    
    I also store a UUID.
    The script now fetches all existing 2D phonons.
    These are not stored here, only the examples are updated with the results
    of this updated script.
    
    Note: the script could very much be improved by implementing a general
    class to traverse the graph via the command-line interface, but at the
    moment this is too much work so I did a lot of copy-paste of the code.

--- misc/data-from-2d-MC-discover/get-data-from-mc-discover.py
+++ misc/data-from-2d-MC-discover/get-data-from-mc-discover.py
@@ -17,6 +17,10 @@ from compute.phononweb.qephonon_qetools import (  # pylint: disable=wrong-import
 MATDYN_EXECUTABLE = os.path.expanduser("~/git/q-e/bin/matdyn.x")
 
 
+class MissingPhononsError(Exception):
+    """Exception raised if no phonons exist for a given compound."""
+
+
 def _prettify_string(name):
     pretty_chars = []
     for char in name:
@@ -71,60 +75,14 @@ def check_matdyn():
 
 def get_files_from_materials_cloud(
     discover_data, compound
-):  # pylint: disable=too-many-locals
+):  # pylint: disable=too-many-locals, too-many-statements
     """Given a compound name, return the content of some relevant files (as a dictionary)."""
     compounds = discover_data["data"]["compounds"]
     material = compounds[compound]
-    bands_uuid = material["bands_2D"]
-    phonons_uuid = material["phonons_2D"]
-
-    # Retrieve PW inputs and outputs
-    api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
-        bands_uuid
-    )
-    inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
-        "incoming"
-    ]
-    bands_pw_uuid = inputs[0]["uuid"]  # There is a single creator
-
-    api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
-        bands_pw_uuid
-    )
-    inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
-        "incoming"
-    ]
-    inputs_dict = {inp["link_label"]: inp["uuid"] for inp in inputs}
-    remote_data_uuid = inputs_dict["parent_calc_folder"]
-
-    api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
-        remote_data_uuid
-    )
-    inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
-        "incoming"
-    ]
-    scf_pw_uuid = inputs[0]["uuid"]  # There is a single creator
-
-    api_content_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/repo/contents?filename=%22aiida.in%22".format(
-        scf_pw_uuid
-    )
-    scf_input_file = urllib.request.urlopen(api_content_url).read()
-    assert (
-        b"calculation = 'scf'" in scf_input_file
-    ), f"The parent calculation does not seem to be a SCF for '{compound}', UUID={scf_pw_uuid}"
-
-    api_outputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/outgoing".format(
-        bands_pw_uuid
-    )
-    outputs = json.loads(urllib.request.urlopen(api_outputs_url).read())["data"][
-        "outgoing"
-    ]
-    outputs_dict = {out["link_label"]: out["uuid"] for out in outputs}
-    folder_data_uuid = outputs_dict["retrieved"]
-
-    api_content_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/repo/contents?filename=%22aiida.out%22".format(
-        folder_data_uuid
-    )
-    scf_output_file = urllib.request.urlopen(api_content_url).read()
+    try:
+        phonons_uuid = material["phonons_2D"]
+    except KeyError:
+        raise MissingPhononsError(f"No phonons for {compound}")
 
     # Retrieve Phonon bands input (matdyn.x)
 
@@ -211,12 +169,125 @@ def get_files_from_materials_cloud(
     matdyn_input_file += "\n".join(new_matdyn_lines)
     matdyn_input_file += "\n"
 
+    # Retrieve PW inputs and outputs
+    api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
+        force_constants_uuid
+    )
+    inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
+        "incoming"
+    ]
+    q2r_uuid = inputs[0]["uuid"]  # There is a single creator
+
+    api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
+        q2r_uuid
+    )
+    inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
+        "incoming"
+    ]
+    inputs_dict = {inp["link_label"]: inp["uuid"] for inp in inputs}
+    remote_data_uuid = inputs_dict["parent_calc_folder"]
+
+    ##
+    api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
+        remote_data_uuid
+    )
+    inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
+        "incoming"
+    ]
+    calcf_uuid = inputs[0]["uuid"]  # There is a single creator
+
+    api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
+        calcf_uuid
+    )
+    inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
+        "incoming"
+    ]
+    inputs_dict = {inp["link_label"]: inp["uuid"] for inp in inputs}
+    # print(compound, inputs_dict.keys())
+    try:
+        remote_data_uuid = inputs_dict[
+            "retrieved_1"
+        ]  # Go to the ph.x of the first q-point
+    except KeyError:
+        # Some materials follow a different path via a different calcfunction, but
+        # the parent of it is still a ph.x so I can just switch which input to follow
+        remote_data_uuid = inputs_dict["ph_folder_with_eps"]
+
+    ##
+    api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
+        remote_data_uuid
+    )
+    inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
+        "incoming"
+    ]
+    ph_uuid = inputs[0]["uuid"]  # There is a single creator
+
+    is_still_ph = True
+    while is_still_ph:
+        api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
+            ph_uuid
+        )
+        inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
+            "incoming"
+        ]
+        inputs_dict = {inp["link_label"]: inp["uuid"] for inp in inputs}
+        remote_data_uuid = inputs_dict["parent_calc_folder"]
+
+        ##
+        api_inputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/incoming".format(
+            remote_data_uuid
+        )
+        inputs = json.loads(urllib.request.urlopen(api_inputs_url).read())["data"][
+            "incoming"
+        ]
+        scf_pw_uuid = inputs[0]["uuid"]  # There is a single creator
+
+        ## We finally got to the SCF!!
+        ## Let's now extract the data.
+
+        api_content_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/repo/contents?filename=%22aiida.in%22".format(
+            scf_pw_uuid
+        )
+        scf_input_file = urllib.request.urlopen(api_content_url).read()
+
+        if b"&inputph" in scf_input_file.lower():
+            # This is still a PH.x calcualtion - probably there were multiple restarts
+            # I mark this as such (and not a SCF pw) and iterate once more in the while loop
+            ph_uuid = scf_pw_uuid
+        else:
+            # I found a non-ph calculation (should be hopefully a PW). I go out of the loop.
+            is_still_ph = False
+
+    assert (
+        b"calculation = 'scf'" in scf_input_file
+    ), f"The parent calculation does not seem to be a SCF for '{compound}', UUID={scf_pw_uuid}"
+
+    api_outputs_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/links/outgoing".format(
+        scf_pw_uuid
+    )
+    outputs = json.loads(urllib.request.urlopen(api_outputs_url).read())["data"][
+        "outgoing"
+    ]
+    outputs_dict = {out["link_label"]: out["uuid"] for out in outputs}
+    folder_data_uuid = outputs_dict["retrieved"]
+
+    api_content_url = "https://aiida.materialscloud.org/2dstructures/api/v4/nodes/{}/repo/contents?filename=%22aiida.out%22".format(
+        folder_data_uuid
+    )
+    scf_output_file = urllib.request.urlopen(api_content_url).read()
+
     return {
         "files": {
             "scf.in": scf_input_file,
             "scf.out": scf_output_file,
             "real_space_force_constants.dat": real_force_constants,
             "matdyn.in": matdyn_input_file.encode("ascii"),
+        },
+        "uuids": {
+            "scf": scf_pw_uuid,
+            "matdyn": matdyn_uuid,
+            "phonon_bands": phonons_uuid,
+            "force_constants": force_constants_uuid,
         },
         "high_symmetry_points": final_high_sym_kpts,
     }
@@ -231,7 +302,28 @@ if __name__ == "__main__":
     )
     discover_data = json.loads(urllib.request.urlopen(discover_url).read())
 
-    for compound in ["AgNO2", "Bi", "BN", "C", "PbI2", "MoS2-MoS2", "P", "PbTe"]:
+    # ["AgNO2", "Bi", "BN", "C", "PbI2", "MoS2-MoS2", "P", "PbTe"]:
+    for compound in sorted(discover_data["data"]["compounds"].keys()):
+
+        # Problematic cases:
+        if compound in [
+            # MISSING PHONONS
+            "CeSiI",
+            "DySBr",
+            "DySI",
+            "ErHCl",
+            "ErSCl",
+            "ErSeI",
+            "NdOBr",
+            "SmOBr",
+            "SmSI",
+            "TbBr",
+            "TbCl",
+            "TmOI",
+            "YbOBr",
+        ]:
+            continue
+
         dest_folder = os.path.join("out-phonons", compound)
 
         ## Now I have all data, I create a folder and store all files
@@ -255,6 +347,8 @@ if __name__ == "__main__":
         for filename, content in compound_info["files"].items():
             with open(os.path.join(dest_folder, filename), "wb") as fhandle:
                 fhandle.write(content)
+        with open(os.path.join(dest_folder, "uuids.json"), "w") as fhandle:
+            json.dump(compound_info["uuids"], fhandle)
         print(f"Files written to folder '{dest_folder}'")
 
         current_dir = os.path.realpath(os.curdir)
